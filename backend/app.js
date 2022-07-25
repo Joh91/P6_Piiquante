4eea70ef4e7811c58fabb871f564bd2c00f8d31f
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-07-25 20:57:19 +0200

    user account function created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 //importation d'Express 
 const express = require('express');
 const app = express();
+app.use(express.json()); 
 
 //connexion aux routes 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 //connexion à la database
 const mongoose = require("mongoose"); 
@@ -14,16 +16,18 @@ mongoose.connect("mongodb+srv://johann:P6_piiquanteDB@cluster0.jeifhdb.mongodb.n
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 
 // Utilisation  du router pour définir les routes 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
 
 //exportation d'Express
 module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,53 @@
+const User = require("../models/User");
+// bcrypt va nous permettre de hasher le mdp  
+const bcrypt = require("bcrypt"); 
+
+const jsonwebtoken = require("jsonwebtoken");
+
+// création de compte 
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        //la fonction renvoie le mail ainsi que le mdp sous forme de hash desormais 
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash,
+            });
+            user.save()
+            .then(() => res.status(201).json({ message: "Utilisateur crée !"})) 
+            .catch(err => res.status(400).json({error})); 
+        })
+        .catch(err => res.status(500).json({error}))
+};
+
+// connexion à un compte existant
+exports.login = (req, res, next) => {
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if (user === null){
+            res.status(401).json({message: "identifiant et/ou mot de pas incorrect(s)"}); 
+        } else {
+            bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if(!valid) {
+                    res.status(401).json({message: "identifiant et/ou mot de pas incorrect(s)"})
+                } else {
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jsonwebtoken.sign(
+                            {userId: user._id},
+                            "RANDOM_TOKEN_SECRET",
+                            {expiresIn: "24h"}
+                        )
+                    }); 
+                }
+            })
+            .catch(err => {
+                res.status(500).json({error});
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error}); 
+    })
+};
