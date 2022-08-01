dbe362a8b2b11a858073fb4f33b49d768799d9a8
johannvilcoq91@gmail.com <johannvilcoq91@gmail.com>
2022-08-01 14:57:19 +0200

    delete fix

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 /* ------ requête POST --------*/ 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
-    
+
     //l'id n'est pas nécessaire car il est directement généré par la BDD
     delete sauceObject._id;
     //l'id en provenance du token sera celui utilisé pour l'authentification pour des raisons de sécurité 
@@ -17,44 +17,26 @@ exports.createSauce = (req, res, next) => {
         ...sauceObject,
         userId: req.auth.userId,
         // multer ne transmet que le nom, nous créeons ainsi l'URL manuellement
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
     });
     console.log(sauce); 
     sauce.save()
-    .then(() => {res.status(201).json({message: 'Sauce créee'})})
+    .then(() => {res.status(201).json({message: 'Création de la sauce'})})
     .catch(error => {res.status(400).json({error})})
 };
 
 /* ----- requête GET (un produit) ------ */ 
 exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({
-    _id: req.params.id
-  }).then(
-    (sauce) => {
-      res.status(200).json(sauce);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
+  Sauce.findOne({ _id: req.params.id})
+  .then((sauce) => {res.status(200).json(sauce)})
+  .catch((error) => {res.status(404).json({error})})
 };
 
 /* ------ requête GET (tous les produits) -------*/ 
 exports.getAllSauce = (req, res, next) => {
-  Sauce.find().then(
-    (sauces) => {
-      res.status(200).json(sauces);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  Sauce.find()
+  .then((sauces) => {res.status(200).json(sauces);})
+  .catch((error) => {res.status(400).json({error: error})})
 };
 
 /*--------- requête PUT ----------*/
@@ -91,20 +73,18 @@ exports.deleteSauce = (req, res, next) => {
       if (sauce.userId != req.auth.userId) {
         res.status(401).json({message: "non-authorisé"});
       } else {
-        const filename = sauce.imageUrl.split("/images/")(1); 
+        const filename = sauce.imageUrl.split("/images/")[1]; 
         fs.unlink(`images/${filename}`, () => {
             sauce.deleteOne({_id: req.params.id})
-            .then(() => {res.status(200).json({message: "objet supprimé !"})})
+            .then(() => {res.status(200).json({message: "produit supprimé !"})})
             .catch(error => res.status(401).json({ error }));
         })
       }
     }
-  ).catch(
+  )
+  .catch(
     (error) => {
-      res.status(500).json({
-        error: error
-      });
-    }
-  );
+      res.status(500).json({error});
+    })
 };
 
